6a4036de8228456be854782a53dce18a5bbd5bad
cesar molina <cesar.molina.torres.05@gmail.com>
2022-02-16 09:53:49 -0500

    add show in ProductController

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -3,9 +3,34 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Interfaces\ProductRepositoryInterface;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    private ProductRepositoryInterface $productRepository;
+
+    public function __construct(ProductRepositoryInterface $productRepository)
+    {
+        $this->productRepository = $productRepository;
+    }
+
+    public function index(): JsonResponse
+    {
+        return response()->json([
+            'data' => $this->entryRepository->getAllEntry()
+        ]);
+    }
+
+
+  public function show(Request $request): JsonResponse
+  {
+       $productId = $request->route('id');
+
+        return response()->json([
+            'data' => $this->productRepository->getProductById($productId)
+        ]);
+  }
+
 }
